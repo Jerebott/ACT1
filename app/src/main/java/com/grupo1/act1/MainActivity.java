95d25f1c406e5f995c4b529324741e3cb09af03d
unknown <Laboratorio@LSC144PCLC0312.alumnos.cst>
2022-09-26 13:17:34 -0300

    Initial commit

---
+++ app/src/main/java/com/grupo1/act1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.grupo1.act1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public EditText nom;
+    public EditText ap;
+    private Button b;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nom = (EditText)findViewById(R.id.Nom);
+        ap = (EditText)findViewById(R.id.Ap);
+        b = (Button) findViewById(R.id.enviar);
+
+    }
+    public void enviar(View a){
+        Intent sgte= new Intent(this,Recibe.class);
+        sgte.putExtra("sends",nom.getText().toString());
+        sgte.putExtra("send2",ap.getText().toString());
+        startActivity(sgte);
+        Toast.makeText(this, "Enviado con exito", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/grupo1/act1/Recibe.java
@@ -0,0 +1,14 @@
+package com.grupo1.act1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recibe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recibe);
+    }
+}
