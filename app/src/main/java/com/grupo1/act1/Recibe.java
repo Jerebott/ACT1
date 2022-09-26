5b0f9a4a77a0bd77a7901f8979946ed4fcc089f8
jere <jeremhrojas@gmail.com>
2022-09-26 19:45:48 -0300

    Se actualizo la segunda actividad para recuperar los extras enviados por el intent

--- app/src/main/java/com/grupo1/act1/Recibe.java
+++ app/src/main/java/com/grupo1/act1/Recibe.java
@@ -3,12 +3,21 @@ package com.grupo1.act1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Recibe extends AppCompatActivity {
+    TextView tx1;
+    TextView tx2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recibe);
+        tx1=(TextView) findViewById(R.id.txnombre);
+        tx2=(TextView) findViewById(R.id.txapellido);
+        String nombre=getIntent().getStringExtra("sends");
+        String apellido=getIntent().getStringExtra("send2");;
+        tx1.setText(nombre);
+        tx2.setText(apellido);
     }
 }
